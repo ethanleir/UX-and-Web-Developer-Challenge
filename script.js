c0483d0d1333ee5cae0cb2af93f91f333b73d03a
Ethan Leir <37488488+ethanleir@users.noreply.github.com>
2021-01-05 20:20:40 -0500

    Update script.js
    Added fading text to nominations menu.

--- script.js
+++ script.js
@@ -38,18 +38,39 @@ function search(){
 //     handleCalls();
 // });
 
-function openNominationsMenu(){
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+async function openNominationsMenu(){
 
     if(open === false){
         document.getElementById("nominationsMenu").style.width = "300px";
         document.getElementById("gray-filter").style.display = "block";
         document.getElementById("gray-filter").style.opacity = "0.5";
+
+        await sleep(350);
+        let nodeList = document.getElementsByClassName("text fade");
+        let i = 0;
+        while (i < nodeList.length){
+            nodeList[i].style.opacity = "1";
+            i++;
+        }
+
         open = true;
     }
     else{
         document.getElementById("nominationsMenu").style.width = "100px";
         document.getElementById("gray-filter").style.opacity = "0";
         document.getElementById("gray-filter").style.display = "none";
+
+        let nodeList = document.getElementsByClassName("text fade");
+        let i = 0;
+        while (i < nodeList.length){
+            nodeList[i].style.opacity = "0";
+            i++;
+        }
+
         open = false;
     }
 
